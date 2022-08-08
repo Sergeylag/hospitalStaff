a0b6899447f04904b52d5350f2266fb194dd3413
Sergeylag <dzirtdo2014@mail.ru>
2022-08-08 09:06:02 +0300

    test commit

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3001
 const merchant_model = require('./merchant_model')
-
+// test push git
 app.use(express.json())
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
